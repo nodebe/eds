36ef3465e48feadef2c3e2a14444b987d091cdf0
Nodebe <nodywelete3@gmail.com>
2021-04-19 10:40:12 +0100

    made changes in config vars

--- eds/__init__.py
+++ eds/__init__.py
@@ -6,8 +6,8 @@ from flask_avatars import Avatars
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] =  'nf2eoiru3204e32nilhrn2olhn'
-app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://postgres:nodywelete1@localhost/edsdb'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
